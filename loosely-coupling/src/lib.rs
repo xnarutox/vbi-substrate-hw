55d6efaa3a1f42223b29bb7b4f0edf289095a78d
le binh <binh.le263@gmail.com>
2022-07-28 17:40:42 +0700

    base exercise 3

---
+++ loosely-coupling/src/lib.rs
@@ -0,0 +1,113 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/v3/runtime/frame>
+pub use pallet::*;
+
+// #[cfg(test)]
+// mod mock;
+
+// #[cfg(test)]
+// mod tests;
+
+// #[cfg(feature = "runtime-benchmarks")]
+// mod benchmarking;
+
+use frame_support::pallet_prelude::*;
+use frame_system::pallet_prelude::*;
+
+use pallet_template::DoSome;
+#[frame_support::pallet]
+pub mod pallet {
+
+	pub use super::*;
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Increase: DoSome;
+
+		//Increase: associated type trong loosely-coupling
+		// DoSome là 1 trait từ pallet-template
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	pub type Something<T> = StorageValue<_, u32>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T:Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		IncreaseSuccess(u32),
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+
+	//extrinsic
+	#[pallet::call]
+	impl<T:Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn increase(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+			//Config::Increase
+			let new_value = T::Increase::increase_value(something);
+
+			// Emit an event.
+			Self::deposit_event(Event::IncreaseSuccess(new_value));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+	}
+}
+
+impl<T:Config> Pallet<T>{
+
+	pub fn update_storage(value:u32) -> DispatchResult{
+		Something::<T>::put(value);
+		Ok(())
+	}
+}
+
+pub trait GetMax{
+	fn get_max_value() -> u32;
+
+}
+
+
+impl<T:Config> GetMax for Pallet<T>{
+	fn get_max_value() -> u32{
+		let value = 3u32;
+		value
+	}
+}
